f706ce1d2ad455436a4c585081e5930e409ea5f7
Bryce Deary <Bryce.Deary@ecosenselighting.com>
2018-01-18 15:05:19 -0800

    fixed some syntax in untest code

--- src/taskCounterClass.c
+++ src/taskCounterClass.c
@@ -1,5 +1,6 @@
 #include "taskCounterClass.h"
 #include <string.h>
+#include "generalDef.h"
 
 
 
@@ -11,25 +12,26 @@ tcHandle_t TCC_AssociateHandle(tcc_t *tcc, tcCounter_t volatile * myCounter,
     return (tcHandle_t)tcc;
 }
 
-void TCC_SignalUp(tcHandle_t task)
+intptr_t TCC_SignalUp(tcHandle_t task)
 {
     if (task)
     {
         // do saturated inc
-        tcCounter_t temp = *(tcc_t*)task->counter + 1;
-        if (temp) *(tcc_t*)task->counter = temp;
-        return;
+    	USAT_INC(*((tcc_t*)task)->counter);
+    	return ((tcc_t*)task)->context;
     }
+    return 0;
 }
 
-void TCC_SignalDown(tcHandle_t task)
+intptr_t TCC_SignalDown(tcHandle_t task)
 {
-    // do saturated dec
-    if (task && *(tcc_t*)task->counter)
-    {
-        *(tcc_t*)task->counter--;
-        return;
-    }
+	if (task)
+	{
+		// do saturated dec
+		USAT_DEC(*((tcc_t*)task)->counter);
+		return ((tcc_t*)task)->context;
+	}
+    return 0;
 }
 
 
@@ -37,17 +39,17 @@ tcCounter_t TCC_Test(tcHandle_t task)
 {
     if (task)
     {
-        return *(tcc_t*)task->counter;
+        return *(((tcc_t*)task)->counter);
     }
     return 0;
 }
 
-tcCounter_t TC_TestAndClear(tcHandle_t task)
+tcCounter_t TCC_TestAndClear(tcHandle_t task)
 {
     if (task)
     {
-        tcCounter_t temp = *(tcc_t*)task->counter;
-        *(tcc_t*)task->counter = 0;
+        tcCounter_t temp = *((tcc_t*)task)->counter;
+        *((tcc_t*)task)->counter = 0;
         return temp;
     }
     return 0;
